1d7ef21afbaa76b5f86150142a228f4bb6855564
tguhxxe <bangguh18@gmail.com>
2024-03-04 14:58:08 +0700

    main

---
+++ Kucing.java
@@ -0,0 +1,16 @@
+public class Kucing {
+    public String nama;
+    public int age;
+    public String address;
+    public String Jenis;
+    public String Kel;
+    
+
+    public void Pisang() {
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : "+ age);
+        System.out.println("Alamat :"+ address);
+        System.out.println("Jenis hewan :"+ Jenis);
+        System.out.println("Jenis Kelamin :"+ Kel);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Kucing Kucing = new Kucing();
+        Kucing.nama = "moci";
+        Kucing.age = 20;
+        Kucing.address = "Jaksel";
+        Kucing.Jenis = "Omnifora";
+        Kucing.Kel = "Rahasia";
+        Kucing.Pisang();
+    }
+}
